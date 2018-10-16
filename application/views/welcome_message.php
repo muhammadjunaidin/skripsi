1a762f259a6eac7a82fef7c1d476fa67971d9383
taufiqade <taufiqadesurya@gmail.com>
2018-10-16 12:47:14 +0700

    landing page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,89 +1,104 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<style type="text/css">
+    <link rel="stylesheet" href="<?=get_css('bootstrap.min.css')?>" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=base_url('assets/font-awesome/css/all.css')?>">
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+    <title>Hello, world!</title>
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
+    <style type="text/css">
+      body{
+        background: #EDF8F0 url(<?=get_image('bg.png')?>) top center repeat-x fixed;
+        color: #033f1c;
+      }
+      .logo {
+        max-width: 150px;
+      }
+      .header {
+        margin: 50px auto;
+        text-align: center;
+        max-width: 650px;
+      }
+      .header-title {
+        display: inline-flex;
+      }
+      .header-title h2 {
+        font-family: "Times New Roman", Times, serif;
+        font-size: 40px;
+        font-weight: bold;
+        text-align: left;
+        color: #07602c;
+      }
+      .intro {
+        text-align: center;
+        margin-bottom: 50px;
+        padding-top: 10px;
+      }
+      .intro h3 {
+        color: #07602c;
+        font-weight: bold;
+      }
+      .feature {
+        padding: 30px 10px;
+      }
+      .feature .col {
+        text-align: center;
+      }
+      .feature i {
+        font-size: 90px;
+      }
+      .detail {
+        margin-top: 20px;
+        padding: 0 20px;
+      }
+      a { color: inherit; }
+    </style>
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <img class="logo" src="<?=get_image('logo-disnaker.png')?>">
+        <div class="header-title">
+          <h2>Pelayanan perizinan <br>Disnaker DIY</h2>
+        </div>
+      </div>
+      <div class="content">
+        <div class="intro">
+          <h3>Selamat datang, di Perizinan online Disnaker DIY</h3>
+          <h5>Sistem perizinan online ini di peruntukkan bagi pemohon yang ingin mengajukan permohonan perizinan secara online</h5>
+        </div>
+        <div class="feature row">
+          <div class="col">
+            <i class="far fa-file-alt"></i>
+            <div class="detail">
+              <a href="registrasi"><h5>Registrasi Akun</h5></a>
+              <p>Layanan ini digunakan untuk pembuatan akun</p>
+            </div>
+          </div>
+          <div class="col">
+            <i class="fas fa-search"></i>
+            <div class="detail">
+              <a href="monitor"><h5>Monitoring Proses</h5></a>
+              <p>Layanan ini digunakan untuk memantau proses perizinan</p>
+            </div>
+          </div>
+          <div class="col">
+            <i class="fas fa-unlock-alt"></i>
+            <div class="detail">
+              <a href="login"><h5>Login</h5></a>
+              <p>Layanan ini digunakan untuk masuk ke aplikasi</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
-<body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
-</body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="<?=base_url('assets/js/bootstrap.min.js')?>"></script>
+  </body>
 </html>
